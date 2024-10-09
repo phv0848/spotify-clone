fd251c59c4bd2ff1af6bedb5a3ea158bfc140b42
phamviethoang020704 <157348044+phamviethoang020704@users.noreply.github.com>
2024-10-09 18:20:57 +0700

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      
     },
   },
   plugins: [],
 };
-export default config;
